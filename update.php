9209626b889df7e4dcb045c80663cc6d277e45c2
Maz <36606334+mynameismaz@users.noreply.github.com>
2018-02-18 20:54:19 +0100

    Added prepared statements in update.php
    Added prepared statements in update.php to improve security.

--- update.php
+++ update.php
@@ -7,42 +7,62 @@
 </head>
 <body>
     <?php
-if(isset($_POST['update']))
-{
     
-    session_start();
-    $name = $_POST['name'];
-    $friend = $_POST['friend'];
-    $isbn = $_POST['isbn'];
-    
-    $servername = "localhost";
-    $username = $_SESSION['username'];
-    $password = $_SESSION['password'];
-    $dbname = "books_lending";
-    $table = "books";
+        if(isset($_POST['update'])) {
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $sql = "INSERT INTO $table (id, name, isbn, friend)
-    VALUES (null, '$name', '$isbn', '$friend')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully <a href='view.php'>return to table</a><br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    
+            session_start();
 
-}else
-{
-    //echo "ddd";
-}
+            // Database connection data.
+            $servername = "localhost";
+            $username = $_SESSION['username'];
+            $password = $_SESSION['password'];
+            $dbname = "books_lending";
+            $table = "books";
 
+            // Connect to mysqli.
+            $mysqli = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection.
+            if (mysqli_connect_errno()) {
+                printf("Connect failed: %s\n", mysqli_connect_error());
+                exit();
+            }
+
+            // Grab data from the form. 
+            $name = $_POST['name'];
+            $isbn = $_POST['isbn'];
+            $friend = $_POST['friend'];
+
+            // Prepare query.
+            $sql = $mysqli->prepare("INSERT INTO $table (name, isbn, friend) VALUES (?, ?, ?)");
+
+            // Bind parameters.
+            $sql->bind_param('sis', $name, $isbn, $friend);
+
+            // If SQL query works fine, execute it, otherwise... show me the error.
+            if ($sql->execute()) {
+                $sql->execute();
+                echo "New record created successfully <a href='view.php'>return to table</a><br>";
+            } else {
+                echo "Error: " . $mysqli->error;
+            }
+
+            // Close the connection.
+            $sql->close();
+
+
+        } else {
+            //echo "ddd";
+        }
 
     ?>
+    
     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" class="login card-panel">
-      name: <input type="text" name="name"><br>
-      friend: <input type="text" name="friend"><br>
-      isbn: <input type="text" name="isbn"><br>
-      <input type="submit" name="update" value="update">
+        name: <input type="text" name="name"><br>
+        friend: <input type="text" name="friend"><br>
+        isbn: <input type="text" name="isbn"><br>
+        <input type="submit" name="update" value="update">
     </form>
+    
 </body>
 </html>
